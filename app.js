ca767022fc260489997ad63e51dd7d01320629b0
Christian Martinez <chmartinez.cba@gmail.com>
2023-12-09 12:32:42 -0300

    [FEAT] Auth - Se crean rutas - Se crean controladores     - Conexion DB     - Creacion usuario     - Verificacion login     - express-session - Se crean vistas
    - Se crea vista de navbar segun estado de session

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const PORT = 3000
+const { config } = require('dotenv')
+config()
+
+/* Configuracion express-session */
+const session = require('express-session')
+app.use(session({
+    secret: 'funko',
+    resave: false,
+    saveUninitialized: true,
+    isLog: false
+}))
+
+/* Override para habilitar los métodos PUT y DELET */
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
+/* Middlewares */
+app.use(express.urlencoded({extended:false})) //Permite obtener la informacion de un formulario
+app.use(express.json()) //Permite leer json - dev
 
 /* Define carpeta de archivos estáticos */
 app.use(express.static(path.resolve(__dirname,'public')))
@@ -18,7 +37,9 @@ const authRouter = require('./src/router/authRouter')
 /* Rutas */
 app.use ('/shop', shopRouter)
 app.use('/auth',authRouter)
-
+app.use('', (req,res,next)=>{
+    res.render('404',view = {title: 'No encontrada',logged: req.session.isLog,glide: false});
+})
 
 
 app.listen(PORT, () => {console.log(`Servidor corriendo en: http://localhost:${PORT}`)})
