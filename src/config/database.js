a12e7e2f136985a063376d525e156b6325ca8529
Exequiel <exemacchi27@gmail.com>
2023-12-09 18:48:58 -0300

    [FIX] Database config
    fix: se elimina el código de configuración local de la base de datos (utilizado para pruebas) y se colocan las necesarias para la base de datos remota.
    remove: se eliminan todos los '.gitkeep' en las carpetas que ya no son necesarios.

--- src/config/database.js
+++ src/config/database.js
@@ -3,23 +3,14 @@
 const { createPool } = require("mysql2/promise");
 
 const pool = createPool({
-    // host: process.env.DB_HOST || "localhost",
-    // user: process.env.DB_USER || "root",
-    // password: process.env.DB_PASSWORD || "",
-    // database: process.env.DB_DATABASE || "funkoshop_db",
-    // waitForConnections: true,
-    // connectionLimit: 5,
-    // queueLimit: 0,
-    // port: process.env.DB_PORT || 3306
-
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "funkoshop_db",
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_DATABASE || "funkoshop_db",
     waitForConnections: true,
     connectionLimit: 5,
     queueLimit: 0,
-    port: 3306
+    port: process.env.DB_PORT || 3306
 });
 
 // Prueba de conexión
