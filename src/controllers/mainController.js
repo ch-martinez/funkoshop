e2d90b141b92aaafb92547991afb320d643fc448
unknown <enrique.ram@gmail.com>
2023-12-09 22:07:02 -0300

    FEAT faqs.ejs/main.ejs/contact.ejs

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,7 +2,7 @@ const mainController= {
     home: (req, res) => res.render("shop/main"),
     contact: (req, res) => res.render("shop/contact"),
     about: (req, res) => res.send("Route for About View"),
-    faqs: (req, res) => res.send("Route for faqs View"),
+    faqs: (req, res) => res.render("shop/faqs"),
 }
 
 module.exports = mainController
