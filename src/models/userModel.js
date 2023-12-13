b7eaaa4aabab66e0e6c612273eda44f15898b947
Christian Martinez <chmartinez.cba@gmail.com>
2023-12-13 15:22:23 -0300

    [FIX] DB conection

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,20 +1,41 @@
 const pool = require("../config/database");
 
 const getPassByEmailFromDB = async (email) => {
-    const [rows] = await pool.query('SELECT name, password, role FROM user WHERE email = ?',[email])
-    return rows[0]
+    try {
+        const [rows] = await pool.query('SELECT name, password, role FROM user WHERE email = ?',[email])
+        return rows[0]
+    } catch (error) {
+        console.error(`Error en la consulta a la base de datos: ${err}`);
+        throw err;
+    }finally{
+        pool.releaseConnection()
+    }
 }
 
 const registerUser = async (userData) => {
-    await pool.query('INSERT INTO user (name, lastname, email, password, role) VALUES ?', [userData])
+    try {
+        await pool.query('INSERT INTO user (name, lastname, email, password, role) VALUES ?', [userData])
+    } catch (error) {
+        console.error(`Error en la consulta a la base de datos: ${err}`);
+        throw err;
+    }finally{
+        pool.releaseConnection()
+    }
 }
 
 const existEmailInDB = async (email) => {
-    const [rows] = await pool.query('SELECT email FROM user WHERE email = ?',[email])
-    if (email == rows[0]?.email) {
-        return true
-    }else{
-        return false
+    try {
+        const [rows] = await pool.query('SELECT email FROM user WHERE email = ?',[email])
+        if (email == rows[0]?.email) {
+            return true
+        }else{
+            return false
+        }
+    } catch (error) {
+        console.error(`Error en la consulta a la base de datos: ${err}`);
+        throw err;
+    }finally{
+        pool.releaseConnection()
     }
 }
 
